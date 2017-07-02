752125ce783575c27d3c320600815006521ec1cf
Manish Kumar <manishyuijaiswal@gmail.com>
2017-07-02 14:29:45 +0530

    EIGHTS

---
+++ EIGHTS.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		long long int k,sum=192;
+		cin>>k;
+		k--;
+		while(k--)
+		sum=sum+250;
+		cout<<sum<<endl;
+	}
+	return 0;
+} 
