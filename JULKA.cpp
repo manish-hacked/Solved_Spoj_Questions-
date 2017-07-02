343cb45a557ba6f3db261b1e287f1d0231bb89d4
Manish Kumar <manishyuijaiswal@gmail.com>
2017-07-02 14:45:56 +0530

    JULKA

---
+++ JULKA.cpp
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.math.BigInteger;
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input = new Scanner(System.in);
+		String total,more;
+		for(int t=0;t<10;t++){
+			total  = input.nextLine();
+			more = input.nextLine();
+			BigInteger total_apples = new BigInteger(total);
+			BigInteger excess_apples = new BigInteger(more);
+			BigInteger ans_1 = total_apples.add(excess_apples).divide(BigInteger.valueOf(2));
+			BigInteger ans_2 = total_apples.subtract(excess_apples).divide(BigInteger.valueOf(2));
+			System.out.println(ans_1);
+            System.out.println(ans_2);
+		}
+	}
+} 
