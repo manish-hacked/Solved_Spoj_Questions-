94c317f07e48b8567cdbec6cdef8428b709266f9
Manish Kumar <manishyuijaiswal@gmail.com>
2017-07-02 15:31:42 +0530

    WILLITST

---
+++ WILLITST.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main(){
+	
+	unsigned long long n;
+	cin>>n;
+	long double f = log2(n);
+	unsigned long long p= f;
+	if(p==f)
+	cout<<"TAK"<<endl;
+	else
+	cout<<"NIE"<<endl;
+} 
